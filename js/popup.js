174d8717f178efaea6d020a42153670e3c93ac9a
Sansara0 <second68@gmail.com>
2018-10-08 17:45:35 +0300

    изменены классы

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,12 @@
-var link = document.querySelector(".js-contacts-btn");
+var link = document.querySelector(".connection-btn");
 var popup = document.querySelector(".contacts__popup");
 var overlay = document.querySelector(".popup-overlay");
 var close = document.querySelector(".close");
-var form = popup.querySelector("form");
+var form = popup.querySelector(".contacts__form");
 var contactsName = popup.querySelector("[name = name-contacts]");
 var contactsEmail = popup.querySelector("[name = email-contacts]");
 var contactsText = popup.querySelector("[name = text-contacts]");
-
+var escKey = 27;
 
 link.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -18,7 +18,7 @@ link.addEventListener("click", function(evt){
 close.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.remove("contacts__popup-show");
-  popup.classList.remove("js-error");
+  popup.classList.remove("error");
   overlay.classList.remove("popup-overlay-show");
 
 });
@@ -32,12 +32,12 @@ overlay.addEventListener("click", function(evt){
 form.addEventListener("submit", function(evt) {
   if (!contactsName.value || !contactsEmail.value || !contactsText.value) {
     evt.preventDefault();
-    popup.classList.add("js-error");
+    popup.classList.add("error");
   }
 })
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  if (evt.keyCode === escKey) {
     if (popup.classList.contains("contacts__popup-show")) {
       evt.preventDefault();
       popup.classList.remove("contacts__popup-show");
